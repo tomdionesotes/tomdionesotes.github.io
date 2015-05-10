ff97383b467fcf4f82ee9946f3cd8606079d1819
Tom Dionesotes <tomdionesotes@users.noreply.github.com>
2015-05-10 06:07:51 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
+ 
  $("#left").mouseenter(function(){
-  $("body").css("background-color", "yellow");
+  $(".body").css("background-color", "#1daeff");
  });
  $("#left").mouseleave(function(){
-  $("body").css("background-color", "green");
+  $(".body").css("background-color", "#7d889a");
  });
+ 
+ "#center").mouseenter(function(){
+  $(".body").css("background-color", "#27bb66");
+ });
+ $("#center").mouseleave(function(){
+  $(".body").css("background-color", "#7d889a");
+ });
+ 
+ "#right").mouseenter(function(){
+  $(".body").css("background-color", "#ff5257");
+ });
+ $("#right").mouseleave(function(){
+  $(".body").css("background-color", "#7d889a");
+ });
+ 
 });
